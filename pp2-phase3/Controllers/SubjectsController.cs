4a95b9dff2c20e9fce844c49e6ca63c3a689defe
Sowmya G <sowmyagubbala0681@gmail.com>
2022-10-30 23:48:46 +0530

    Add project files.

---
+++ pp2-phase3/Controllers/SubjectsController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pp2_phase3.Controllers
+{
+    public class SubjectsController : Controller
+    {
+        // GET: Subjects
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Subjects/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Subjects/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Subjects/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Subjects/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Subjects/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Subjects/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Subjects/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ pp2-phase3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="pp2_phase3.MvcApplication" Language="C#" %>
